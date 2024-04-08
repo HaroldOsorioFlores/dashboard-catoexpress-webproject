0d629a8e4e98426ccb2009b4022b797bde957b76
HaroldOsorioFlores <haroldosorio32@gmail.com>
2024-04-07 22:44:07 -0500

    [MOD] modify logo catoexpress

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -5,8 +5,8 @@ import { CatoexpressLogo, LoginFormModal } from "@/components";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-green-700 p-4 md:h-52">
-        <CatoexpressLogo className="h-[8rem] w-[16rem]" fill="#FFFFFF" />
+      <div className="flex h-24 shrink-0 items-end rounded-lg bg-green-700 p-4 md:h-52">
+        <CatoexpressLogo className="md:h-[8rem] md:w-[16rem] h-[4rem] w-[8rem] " fill="#FFFFFF" />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
